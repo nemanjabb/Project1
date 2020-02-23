235292c92ffa0ea3362ee243775f391646b0d0fc
Nemanja Djuricic <nemanja.djuricic@tradeticityservices.com>
2020-02-23 20:05:29 +0100

    Commit3

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -2,14 +2,25 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class App {
-	
-	public static void main (String[] args) {
-		
-		SpringApplication.run(App.class, args); 
-		
+public class App extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
 	}
-	
+}
+
+@RestController
+class HelloController {
+
+	@RequestMapping("/")
+	String hello() {
+
+		return "Hi !";
+
+	}
 }
